8c40a020e33f7637d60df86ba284bd17a31e2f61
OudeaBrieg <brieg.oudea@epita.fr>
2022-07-13 21:33:56 +0200

    [ARGS] new args parser for run.py added

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import sys
+import argparse
+from argparse import RawTextHelpFormatter
+
 import numpy as np
 from torch.nn import Tanh
 
@@ -27,25 +30,81 @@ if sys.version_info[0] != 3 or sys.version_info[1] != 7 or sys.version_info[2] !
 
 
 if __name__ == '__main__':  # Required for multiprocessing
-    frame_skip = 8          # Number of ticks to repeat an action
-    half_life_seconds = 5   # Easier to conceptualize, after this many seconds the reward discount is 0.5
+    parser = argparse.ArgumentParser(
+        description=f'Personalized Gym for training PPO-based Rocket League Reinforcement \
+                      Learning Agents (e.g. : a Dojo for Rocket-powered Soccer playing \
+                      Vehicles).',
+        formatter_class=RawTextHelpFormatter)
+
+    # In-Game Metadata
+    parser.add_argument('-agents_per_match', type=int, default=6,
+                        help='Number of Agents per Instance\n' + \
+                             '(1 if solo, 2 if 1v1, 4 if 2v2, 6 if 3v3)')
+    parser.add_argument('-spawn_opponents', type=bool, default=True,
+                        help='Enabling opponents to spawn')
+    parser.add_argument('-team_size', type=int, default=3,
+                        help='Number of Agents per Team\n' + \
+                             '(1 if solo, 2 if twos, 3 if threes)')
+    
+    # Training Session Parameters
+    parser.add_argument('-num_instances', type=int, default=1,
+                        help='Number of Training Instances to be run in parallel')
+    parser.add_argument('-device', type=str, default='auto',
+                        help='Device (cpu, cuda, ...) on which the code should be run\n' + \
+                             '(Setting it to auto, the code will run on GPU if possible)')
+    parser.add_argument('-frame_skip', type=int, default=8,
+                        help='Number of Ticks to repeat an action')
+    parser.add_argument('-half_life_seconds', type=int, default=5,
+                        help='Number of Seconds until Half-life\n' + \
+                             '(After this many seconds the reward discount is 0.5)')
+
+    # Model Hyperparameters
+    parser.add_argument('-learning_rate', type=float, default=5e-5,
+                        help='The Learning Rate\n' + \
+                             '(Around 5e-5 is fairly common for PPO)')
+    parser.add_argument('-target_steps', type=int, default=1_000_000,
+                        help='Number of Steps we want to train for each training session\n' + \
+                             '(Represents 10 times the batch size)')
+    parser.add_argument('-n_epochs', type=int, default=10,
+                        help='Number of epoch when optimizing the surrogate loss\n' + \
+                             '(PPO recommmends multiple epochs)')
+    parser.add_argument('-ent_coef', type=float, default=0.01,
+                        help='Entropy coefficient for the loss calculation\n' + \
+                             '(PPO Atari recommmends setting it to 0.01)')
+    parser.add_argument('-v_coef', type=float, default=1.,
+                        help='Value function coefficient for the loss calculation\n' + \
+                             '(PPO Atari recommmends setting it to 1)')
+
+    # Saving Paths
+    parser.add_argument('-model_path', type=str, default='models',
+                        help='Relative Path to save the Trained Model')
+    parser.add_argument('-model_name', type=str, default='bot_michel',
+                        help='Name of the Model')
+    parser.add_argument('-logs_path', type=str, default='logs',
+                        help='`python -m tensorboard.main --logdir=your-path` at the root.')
+    
+    args = parser.parse_args()
+
+    frame_skip = args.frame_skip                # Number of ticks to repeat an action
+    half_life_seconds = args.half_life_seconds  # Easier to conceptualize, after this many seconds the reward discount is 0.5
 
     fps = 120 / frame_skip
-    gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))  # Quick mafs
-    agents_per_match = 6        # 2 if 1v1, 4 if 2v2, 6 if 3v3
-    num_instances = 4           # As many as you can handle
-    target_steps = 1_000_000    # How many steps we want to train for each training session
-    steps = target_steps // (num_instances * agents_per_match) # Making sure the experience counts line up properly
-    batch_size = target_steps//10 # getting the batch size down to something more manageable - 100k in this case
+    gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))     # Quick mafs
+    agents_per_match = args.agents_per_match                    # 2 if 1v1, 4 if 2v2, 6 if 3v3
+    num_instances = args.num_instances                          # As many as you can handle
+    target_steps = args.target_steps                            # How many steps we want to train for each training session
+    steps = target_steps // (num_instances * agents_per_match)  # Making sure the experience counts line up properly
+    batch_size = target_steps//10                               # Getting the batch size down to something more manageable - 100k in this case
     training_interval = 25_000_000
     mmr_save_frequency = 50_000_000
+    model_save_path = f"{args.model_path}/{args.model_name}"
 
-    def exit_save(model):
-        model.save("models/bot_michel")
+    def exit_save(model, path):
+        model.save(path)
 
-    def get_match():  # Need to use a function so that each instance can call it and produce their own objects
+    def get_match():                                            # Need to use a function so that each instance can call it and produce their own objects
         return Match(
-            team_size=3,
+            team_size=args.team_size,
             tick_skip=frame_skip,
             reward_function=CombinedReward(
             (
@@ -63,28 +122,25 @@ if __name__ == '__main__':  # Required for multiprocessing
                 ),
             ),
             (0.1, 1.0, 1.0, 1.0)),
-            # if rlgym < 1.2, 'self_play'=True instead of spawn_opponents=True
-            spawn_opponents=True,
+            spawn_opponents=args.spawn_opponents,
             terminal_conditions=[TimeoutCondition(fps * 300), NoTouchTimeoutCondition(fps * 45), GoalScoredCondition()],
-            obs_builder=AdvancedObs(),  # Not that advanced, good default
-            #state_setter=DefaultState(),  # Resets to kickoff position
-            state_setter=BetterRandom(), #Resets to a random position
+            obs_builder=AdvancedObs(),      # Not that advanced, good default
+            state_setter=BetterRandom(),    # Resets to a random position
             action_parser=DiscreteAction()  # Discrete > Continuous don't @ me
         )
 
-    env = SB3MultipleInstanceEnv(get_match, num_instances)            # Start num_instances instances, waiting 60 seconds between each
-    env = VecCheckNan(env)                                # Optional
-    env = VecMonitor(env)                                 # Recommended, logs mean reward and ep_len to Tensorboard
-    env = VecNormalize(env, norm_obs=False, gamma=gamma)  # Highly recommended, normalizes rewards
+    env = SB3MultipleInstanceEnv(get_match, num_instances)  # Start num_instances instances, waiting 60 seconds between each
+    env = VecCheckNan(env)                                  # Optional
+    env = VecMonitor(env)                                   # Recommended, logs mean reward and ep_len to Tensorboard
+    env = VecNormalize(env, norm_obs=False, gamma=gamma)    # Highly recommended, normalizes rewards
 
     try:
-        model = PPO.load(
-            "models/bot_michel.zip",
-            env,
-            device="auto",
-            custom_objects={"n_envs": env.num_envs}, #automatically adjusts to users changing instance count, may encounter shaping error otherwise
-            # If you need to adjust parameters mid training, you can use the below example as a guide
-            #custom_objects={"n_envs": env.num_envs, "n_steps": steps, "batch_size": batch_size, "n_epochs": 10, "learning_rate": 5e-5}
+        model = PPO.load(f"{model_save_path}.zip",
+                         env,
+                         device="auto",
+                         custom_objects={"n_envs": env.num_envs}, # Automatically adjusts to users changing instance count, may encounter shaping error otherwise
+                         # If you need to adjust parameters mid training, you can use the below example as a guide
+                         # custom_objects={"n_envs": env.num_envs, "n_steps": steps, "batch_size": batch_size, "n_epochs": 10, "learning_rate": 5e-5}
         )
         print("Loaded previous exit save.")
         #total_params = count_parameters(model)
@@ -98,17 +154,17 @@ if __name__ == '__main__':  # Required for multiprocessing
         model = PPO(
             MlpPolicy,
             env,
-            n_epochs=10,                 # PPO calls for multiple epochs
+            n_epochs=args.n_epochs,               # PPO calls for multiple epochs
             policy_kwargs=policy_kwargs,
-            learning_rate=5e-5,          # Around this is fairly common for PPO
-            ent_coef=0.01,               # From PPO Atari
-            vf_coef=1.,                  # From PPO Atari
-            gamma=gamma,                 # Gamma as calculated using half-life
-            verbose=3,                   # Print out all the info as we're going
-            batch_size=batch_size,             # Batch size as high as possible within reason
-            n_steps=steps,                # Number of steps to perform before optimizing network
-            tensorboard_log="logs",  # `python -m tensorboard.main --logdir=logs` in terminal to see graphs
-            device="auto"                # Uses GPU if available
+            learning_rate=args.learning_rate,     # Around this is fairly common for PPO
+            ent_coef=args.ent_coef,               # From PPO Atari
+            vf_coef=args.v_coef,                  # From PPO Atari
+            gamma=gamma,                          # Gamma as calculated using half-life
+            verbose=3,                            # Print out all the info as we're going
+            batch_size=batch_size,                # Batch size as high as possible within reason
+            n_steps=steps,                        # Number of steps to perform before optimizing network
+            tensorboard_log=args.logs_path,       # `python -m tensorboard.main --logdir=logs` in terminal to see graphs
+            device=args.device                    # Uses GPU if available
         )
         #total_params = count_parameters(model)
         
@@ -116,21 +172,21 @@ if __name__ == '__main__':  # Required for multiprocessing
     # Save model every so often
     # Divide by num_envs (number of agents) because callback only increments every time all agents have taken a step
     # This saves to specified folder with a specified name
-    callback = CheckpointCallback(round(5_000_000 / env.num_envs), save_path="models", name_prefix="rl_model")
+    callback = CheckpointCallback(round(5_000_000 / env.num_envs), save_path=args.model_path, name_prefix="rl_model")
 
     try:
         mmr_model_target_count = model.num_timesteps + mmr_save_frequency
         while True:
             #may need to reset timesteps when you're running a different number of instances than when you saved the model
             model.learn(training_interval, callback=callback, reset_num_timesteps=False) #can ignore callback if training_interval < callback target
-            model.save("models/bot_michel")
+            model.save(model_save_path)
             if model.num_timesteps >= mmr_model_target_count:
-                model.save(f"mmr_models/bot_michel_{model.num_timesteps}")
+                model.save(f"mmr_models/{args.model_name}_{model.num_timesteps}")
                 mmr_model_target_count += mmr_save_frequency
 
     except KeyboardInterrupt:
         print("Exiting training")
 
     print("Saving model")
-    exit_save(model)
+    exit_save(model, model_save_path)
     print("Save complete")
