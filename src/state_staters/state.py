16a0ba55a56d33eacc99f469ca22fc1aff294313
OudeaBrieg <brieg.oudea@epita.fr>
2022-07-16 21:02:06 +0200

    [TRAIN] Distance state commented

--- src/state_staters/state.py
+++ src/state_staters/state.py
@@ -42,6 +42,7 @@ class DistanceState(StateSetter):  # Random state with some triangular distribut
         self.give_boost = give_boost
 
     def reset(self, state_wrapper: StateWrapper):
+        # Computing Distance from Car to Ball
         def difficulty_distance(difficulty):
             def distance_easy():
                 return np.random.uniform(-128, -1024)
@@ -60,31 +61,43 @@ class DistanceState(StateSetter):  # Random state with some triangular distribut
 
         # Ball Initialization
         if self.ball_on_ground:
-            state_wrapper.ball.set_pos(0, 0, 0)     # Set Ball Position
+            # Ball on the ground
+            ball_height = 0    
         else:
-            state_wrapper.ball.set_pos(0, 0, np.random.uniform(90, 642)) # Set Ball Position
-        state_wrapper.ball.set_lin_vel(0, 0, 0)     # Set Ball Linear Velocity
-        state_wrapper.ball.set_ang_vel(0, 0, 0)     # Set Ball Angular Velocity
+            # Ball in the air
+            ball_height = np.random.uniform(90, 642)
+        # Set Ball Position
+        state_wrapper.ball.set_pos(0, 0, ball_height)
+        # Set Ball Linear Velocity
+        state_wrapper.ball.set_lin_vel(0, 0, 0)
+        # Set Ball Angular Velocity
+        state_wrapper.ball.set_ang_vel(0, 0, 0)     
         
         # Cars Initialization
         for car in state_wrapper.cars:
-            distance = difficulty_distance(self.difficulty)   # Computing Distance from Car to Ball
+            distance = difficulty_distance(self.difficulty)
             if self.cars_on_ground:
+                # Car on the ground
                 car_height = 0 
             else:
+                # Car in the air
                 car_height = np.random.uniform(90, 642)
+            # Set Car Position
             car.set_pos(0, distance, car_height)
-                
-
+            face_ball = (np.pi/2)
             yaw_treshold = (random.random() - 0.5) * (np.pi * 0.15)
-            car.set_rot(0, (np.pi/2) + yaw_treshold, 0) # Set Car Rotation at (0, pi/2 +th, 0)
-            
-            car.set_lin_vel(0, 0, 0)                    # Set Car Linear Velocity to (0, 0, 0)
-            car.set_ang_vel(0, 0, 0)                    # Set Car Angular Velocity to (0, 0, 0)
-            
+            # Set Car Rotation
+            car.set_rot(0, face_ball + yaw_treshold, 0) 
+            # Set Car Linear Velocity
+            car.set_lin_vel(0, 0, 0)  
+            # Set Car Angular Velocity                  
+            car.set_ang_vel(0, 0, 0)
+            # Set Car Boost
             if self.give_boost:
+                # Give Random Boost from 12 -> 100
                 car.boost = np.random.uniform(0.12, 1.00)
             else:
+                # Give No Boost
                 car.boost = 0
 
     
